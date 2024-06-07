aeed3903d28e0d59d73843688c3b9dbfc10ac548
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-06-07 15:18:42 +0700

    feat: Find a Book GANG

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [books, setBooks] = useState([]);
+  const [query, setQuery] = useState("");
+
+  const findBook = async (searchQuery) => {
+    try {
+      const response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${searchQuery}`
+      );
+      setBooks(response.data.items);
+    } catch (error) {
+      console.log("Error fetching the books", error);
+    }
+  };
+
+  useEffect(() => {
+    if (query) {
+      findBook(query);
+    }
+  }, [query]);
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input
+        type="text"
+        id="find-a-book"
+        value={query}
+        onChange={handleInputChange}
+      />
+      <div>
+        <ul>
+          {books.map((book) => (
+            <li key={book.id}>{book.volumeInfo.title}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
