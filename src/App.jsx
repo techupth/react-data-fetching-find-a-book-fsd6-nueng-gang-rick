71dfbf29cd7c3db5dc43e2b93064247d2b967f1a
nueng <wongsaokrn.kump@gmail.com>
2024-06-07 15:03:26 +0700

    feat:make search book

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  let [search, setSearch] = useState("");
+  let [book, setBook] = useState([]);
+  let getSearch = async () => {
+    let searchs = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${search}`
+    );
+    setBook(searchs.data.items);
+  };
+  useEffect(() => {
+      getSearch();
+  }, [search]);
+  let searchBook = (event) => {
+    setSearch(event.target.value);
+  };
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input
+        type="text"
+        value={search}
+        onChange={searchBook}
+        placeholder="Search for a book"
+      />
+      <ul>
+      {book.map((item,index) => {
+        return (
+          
+            <li key={index}>{item.volumeInfo.title}</li>
+        
+        );
+      })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
+
+
